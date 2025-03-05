5ddf8fa0b9eb9435e0700d9953211d013c6d39b7
Rizki Alkarim <rizkiam75@gmail.com>
2025-03-05 11:47:28 +0700

    feat:create image preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import Navbar from "./components/navbar.tsx"
 import FileUploader from "./components/file-uploader.tsx"
 import Container from "./components/container.tsx"
+import EditMenu from "./components/edit-menu.tsx"
+import {useState, useEffect} from "react"
 
 export default function App() {
+  const [file, setFile] = useState(null)
+
+  function handleFileChange(imageData){
+    setFile(imageData)
+  } 
+
   return (
-    <>
       <Navbar />
-      <Container>
-        <FileUploader/>
+      <Container> 
+        {
+          !file ? <FileUploader handleFileChange={handleFileChange} /> : <EditMenu image={file}/>
+        }
       </Container>
-    </>
   )
 }
--- src/components/file-uploader.tsx
+++ src/components/file-uploader.tsx
@@ -1,13 +1,20 @@
-export default function FileUploader(){
+export default function FileUploader({handleFileChange}){
+  
+  function loadImage(event){
+    const image = event.target.files[0]
+
+    if(event.target.files && event.target.files[0]) {
+      handleFileChange(URL.createObjectURL(image))
+    }
+  }
+
   return (
     <div className="p-4 border-solid border-black border-2 border-b-8 border-r-8 rounded-md">
-      <div className="p-32 border-dashed border-black border-2">
-      {/*
-      <button className="px-4 py-2 bg-black text-white rounded font-semibold">
+      <div className="relative p-32 border-dashed border-black border-2 hover:border-blue-500">
+        <button className="px-4 py-2 bg-black text-white rounded font-semibold hover:bg-blue-500">
           Unggah Gambar
         </button>
-      */}  
-        <input type="file" className="px-4 py-2 bg-black text-white rounded font-semibold text-center"/>
+        <input type="file" accept=".png, .jpg, .jpeg" className="px-4 py-2 bg-black text-white rounded font-semibold text-center absolute opacity-0 inset-0" onChange={loadImage}/>
       </div>
     </div>
   )
