92ddebe1e8c02539155fa65035b3745a2326406d
Rizki Alkarim <rizkiam75@gmail.com>
2025-03-06 21:13:18 +0700

    feat: add interactivity to crop menu and background selection menu

--- src/components/crop-tool.tsx
+++ src/components/crop-tool.tsx
@@ -1,14 +1,14 @@
 import MenuNavigation from "./menu-navigation.tsx"
 
-export default function CropTool(){
+export default function CropTool({handleRatio, ratio}){
   return(
       <div className="flex flex-col gap-6">
         <div className="p-4 rounded border-solid border-1">
           <p className="text-black font-semibold">Pilih rasio:</p>
           <fieldset className="flex gap-2 items-center text-lg">
-              <input id="46" type="radio" className="appearance-none checked:border-2 w-4 h-4 rounded bg-white border-solid border-black border-2 checked:bg-blue-500" name="background-color"/>
+              <input id="46" type="radio" className="appearance-none checked:border-2 w-4 h-4 rounded bg-white border-solid border-black border-2 checked:bg-blue-500" name="background-color" checked={ratio == "46"} onChange={() => handleRatio("46")}/>
               <label htmlFor="46">4:6</label>
-              <input id="34" type="radio" className="appearance-none checked:border-2 w-4 h-4 rounded bg-white border-solid border-black border-2 checked:bg-blue-500" name="background-color"/>
+              <input id="34" type="radio" className="appearance-none checked:border-2 w-4 h-4 rounded bg-white border-solid border-black border-2 checked:bg-blue-500" name="background-color" checked={ratio == "34"} onChange={() => handleRatio("34")}/>
             <label htmlFor="34">3:4</label>
           </fieldset>
         </div>
--- src/components/edit-menu.tsx
+++ src/components/edit-menu.tsx
@@ -4,12 +4,14 @@ import CropTool from "./crop-tool.tsx"
 import FileUploader from "./file-uploader.tsx"
 import FileDownloader from "./file-downloader.tsx"
 import MenuNavigation from "./menu-navigation.tsx"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 export default function EditMenu(){
   
   const [file, setFile] = useState(null)
   const [currentMenu, setCurrentMenu] = useState("crop")
+  const [ratio, setRatio] = useState(null)
+  const [bgColor, setBgColor] = useState(null)
 
   function handleFileChange(imageData){
     setFile(imageData)
@@ -18,7 +20,15 @@ export default function EditMenu(){
   function handleCurrentMenu(currentMenu){
     setCurrentMenu(currentMenu)
   }
-  
+
+  function handleRatio(selectedRatio){
+    setRatio(selectedRatio)
+  }
+
+  function handleBgColor(selectedBgColor){
+    setBgColor(selectedBgColor)
+  }
+ 
   return(
     <div className="flex justify-betwwen items-end gap-12 p-12 rounded-md border-solid border-2 border-b-8 border-r-8 border-black min-w-[40vw]">
     {
@@ -28,8 +38,8 @@ export default function EditMenu(){
           <Preview image={file}/>
           <div className="flex flex-col gap-4 min-w-max">
             {
-              currentMenu == "crop" ? <CropTool/> :
-              currentMenu == "bg-selection" ? <BgSelector/> :
+              currentMenu == "crop" ? <CropTool handleRatio={handleRatio} ratio={ratio}/> :
+              currentMenu == "bg-selection" ? <BgSelector handleBgColor={handleBgColor} bgColor={bgColor}/> :
               <FileDownloader/>
             }
             <MenuNavigation currentMenu={currentMenu} handleCurrentMenu={handleCurrentMenu}/>
