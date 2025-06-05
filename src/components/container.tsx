e19ec1dec4a3133841232c8e718eed156d4ab385
Rizki Alkarim <rizkiam75@gmail.com>
2025-06-05 23:04:36 +0700

    fix: screen and canvas size

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,6 @@
 export default function Container({children}: {children: React.ReactNode}){
   return(
-    <div className="flex justify-center items-center lg:items-center h-screen">
+    <div className="flex justify-center items-center lg:items-center min-h-[90vh]">
       {children}
     </div>
   )
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -11,7 +11,7 @@ export default function Preview({imageRef, canvasRef, image, currentMenu}: Props
   return(
     <div className="flex">
       <img className={`max-h-[50vh] h-full w-full block inset-0 ${currentMenu == "edit" ? "block" : "hidden"}`} src={image} ref={imageRef}/>
-      <canvas className={`min-h-[50vh] max-h-[50vh] h-full self-end ${currentMenu == "edit" ? "hidden" : "block"}`} width="300" height="400" ref={canvasRef}></canvas>
+      <canvas className={`max-w-[50vw] lg:max-h-[50vh] h-full self-end ${currentMenu == "edit" ? "hidden" : "block"}`} width="300" height="400" ref={canvasRef}></canvas>
     </div>
   )
 }
